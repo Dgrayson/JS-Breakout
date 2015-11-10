b044263005671836249dfc642bb69d05f0b3e239
Darayavaus Grayson <dgrayson100@gmail.com>
2015-11-10 13:32:06 -0500

    Formatted code and added comments

--- game.js
+++ game.js
@@ -6,20 +6,20 @@ var blocks = [];
 var colors = ['red', 'blue', 'yellow', 'pink', 'purple']; 
 
 var Player = function(x, y){
-	this.x = x; 
+	this.x = x - 50; 
 	this.y = y; 
 	this.width = 100; 
 	this.height = 20; 
-
-	this.drawPlayer = function(){
-
-		ctx.fillStyle = "red";
-		ctx.beginPath(); 
-		ctx.rect(this.x, this.y, this.width, this.height); 
-		ctx.closePath(); 
-		ctx.fill(); 
-	}
 }
+
+Player.prototype.drawPlayer = function(){
+
+	ctx.fillStyle = "red";
+	ctx.beginPath(); 
+	ctx.rect(this.x, this.y, this.width, this.height); 
+	ctx.closePath(); 
+	ctx.fill(); 
+};
 
 var Block = function(x, y, color){
 	this.x = x; 
@@ -27,88 +27,100 @@ var Block = function(x, y, color){
 	this.width = 40; 
 	this.height = 20;
 	this.color = color;  
+};
 
-	this.drawBlock = function(){
-		ctx.fillStyle = color; 
-		ctx.beginPath(); 
-		ctx.rect(this.x, this.y, this.width, this.height); 
-		ctx.closePath(); 
-		ctx.fill(); 
-	}
-}
+Block.prototype.drawBlock = function(){
+	ctx.fillStyle = color; 
+	ctx.beginPath(); 
+	ctx.rect(this.x, this.y, this.width, this.height); 
+	ctx.closePath(); 
+	ctx.fill(); 
+};
 
 var Ball = function(){
 	this.x = c.width /2; 
 	this.y = c.height /2;
 	this.radius = 10; 
 
-	var dx = 2; 
-	var dy = -2; 
+	this.dx = 2; 
+	this.dy = -2; 
+};
 
-	this.drawBall = function(){
-		ctx.beginPath(); 
-		ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI, false); 
-		ctx.fillStyle = "red"; 
-		ctx.fill(); 
-		ctx.lineWidth = 2; 
-		ctx.strokeStyle = '#000000'; 
-		ctx.stroke(); 
+Ball.prototype.drawBall = function(){
+	ctx.beginPath(); 
+	ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI, false); 
+	ctx.fillStyle = "red"; 
+	ctx.fill(); 
+	ctx.lineWidth = 2; 
+	ctx.strokeStyle = '#000000'; 
+	ctx.stroke(); 
+};
+
+Ball.prototype.moveBall = function(){
+	this.x += this.dx; 
+	this.y += this.dy; 
+};
+
+Ball.prototype.collisionCheck = function(){
+	var i = 0; 
+
+	// Check if ball has collided with left or right wall
+	if(this.x + this.radius >= c.width || this.x + this.radius <= 0){
+		this.dx  = this.dx * -1; 
 	}
 
-	this.moveBall = function(){
-		this.x += dx; 
-		this.y += dy; 
+	// check if ball has collided with top or bottom wall
+	if(this.y + this.radius >= c.height || this.y + this.radius <= 0){
+		this.dy = this.dy * -1; 
 	}
 
-	this.collsionCheck = function(){
+	// Paddle collision
+	if(this.x + this.radius > p.x && this.x < p.x + p.width && this.y + this.radius > p.y
+		&& this.y < p.y + p.height)
+	{
+		//this.dx = this.dx * -1; 
+		this.dy = this.dy * -1; 
+	}
 
-		var i = 0; 
 
-		if(this.x + this.radius >= c.width || this.x + this.radius <= 0){
-			dx  = dx * -1; 
-		}
-
-		if(this.y + this.radius >= c.height || this.y + this.radius <= 0){
-			dy = dy * -1; 
-		}
-
-		if(this.x + this.radius > p.x && this.x < p.x + p.width && this.y + this.radius > p.y
-			&& this.y < p.y + p.height)
-		{
-			//dx = dx * -1; 
-			dy = dy * -1; 
-		}
-
-		while(i < blocks.length){
+	// Loop through blocks array to check for collisions
+	while(i < blocks.length){
 
 		if(this.x + this.radius > blocks[i].x && this.x < blocks[i].x + blocks[i].width && this.y + this.radius > blocks[i].y
-			&& this.y < blocks[i].y + blocks[i].height)
-			{
+			&& this.y < blocks[i].y + blocks[i].height){
 			 
-				dy = dy * -1; 
+			this.dy = this.dy * -1; 
 
 
-				// Remove block from array
-				blocks.splice(i, 1);  
-			}
-
-			i++; 
+			// Remove block from array
+			blocks.splice(i, 1);  
 		}
-	}
-}
-// Initialize Game
 
+		i++; 
+	}
+};
+
+// Initialize Game
 function init(){
 
 	c = document.getElementById("myCanvas"); 
 	ctx = c.getContext("2d"); 
-	ctx.translate(0.1, 0.1); 
-	p = new Player(250, 550); 
+	p = new Player(c.width / 2, 550); 
 	b = new Ball(); 
 	createBlocks(); 
 	setInterval(gameLoop, 10); 
-}
+};
 
+
+// restart game on game over
+function restartGame(){
+	p = new Player(c.width / 2, 550); 
+	b = new Ball(); 
+	createBlocks(); 
+};
+
+
+// Create block objects and fill blocks array
 function createBlocks(){
 
 	var i, j; 
@@ -128,10 +140,11 @@ function createBlocks(){
 		}
 
 		z++; 
-		y += 40; 
+		y += 30; 
 	}
-}
+};
 
+// Draw blocks on canvas
 function drawBlocks(){
 	var i = 0; 
 	while(i < blocks.length){
@@ -142,28 +155,27 @@ function drawBlocks(){
 		ctx.fill(); 
 		i++; 
 	}
-}
+};
 
+
+// Main game loop
 function gameLoop(){
-
-	console.log("Running"); 
-
 	clear(); 
 
 	drawBlocks(); 
 	p.drawPlayer(); 
 	b.drawBall(); 
 	b.moveBall(); 
-	b.collsionCheck();
-}
+	b.collisionCheck();
+};
 
+// Clear canvas
 function clear(){
 	ctx.clearRect(0,0,500,600); 
-}
+};
 
+// Handles keyboard input
 function getKeyDown(e){
-
-	console.log("key pressed"); 
 
 	switch(e.keyCode){
 
@@ -176,6 +188,6 @@ function getKeyDown(e){
 			p.x += 10;  
 			break; 
 	}
-}
+};
 
 window.addEventListener('keydown', getKeyDown, true); 
